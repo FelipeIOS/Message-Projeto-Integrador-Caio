ecd4dc4f9b66c4f094b0e6c5d68eee7d0cadf9a7
Caio Fabrini <caiofabrini@outlook.com>
2021-02-21 12:15:19 -0300

    terminei od ajustes de UI da tela de register

--- Message/Message/View/Register/RegisterScreen.swift
+++ Message/Message/View/Register/RegisterScreen.swift
@@ -26,7 +26,50 @@ class RegisterScreen: UIView {
       let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Adicionar Foto", for: .normal)
-        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: CustomFont.poppinsBold, size: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(self.tappedAddPhotoButton), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var emailTextField:UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = .white
+        tf.layer.borderWidth = 1.5
+        tf.layer.cornerRadius = 7.5
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.keyboardType = .emailAddress
+        tf.placeholder = "Email"
+        tf.font = UIFont(name: CustomFont.poppinsBold, size: 14)
+        tf.textColor = .darkGray
+        return tf
+    }()
+    
+    lazy var passwordTextField:UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = .white
+        tf.clipsToBounds = true
+        tf.layer.borderWidth = 1.5
+        tf.layer.cornerRadius = 7.5
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.placeholder = "Senha"
+//        tf.isSecureTextEntry = true
+        tf.font = UIFont(name: CustomFont.poppinsBold, size: 14)
+        tf.textColor = .darkGray
+        return tf
+    }()
+    
+    lazy var registerUserButton:UIButton = {
+      let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Cadastrar", for: .normal)
+        button.titleLabel?.font = UIFont(name: CustomFont.poppinsBold, size: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 7.5
+        button.backgroundColor = UIColor(red: 3/255, green: 58/255, blue: 51/255, alpha: 1.0)
         button.addTarget(self, action: #selector(self.tappedAddPhotoButton), for: .touchUpInside)
         return button
     }()
@@ -34,9 +77,12 @@ class RegisterScreen: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .white
+        self.backgroundColor = UIColor(red: 24/255, green: 117/255, blue: 104/255, alpha: 1.0)
         self.addSubview(self.imageAddUser)
         self.addSubview(self.addImageButton)
+        self.addSubview(self.emailTextField)
+        self.addSubview(self.passwordTextField)
+        self.addSubview(self.registerUserButton)
         self.setUpConstraints()
     }
     
@@ -59,7 +105,22 @@ class RegisterScreen: UIView {
             self.addImageButton.topAnchor.constraint(equalTo: self.imageAddUser.bottomAnchor, constant: CGFloat(12)),
             self.addImageButton.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: CGFloat(20)),
             self.addImageButton.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: CGFloat(-20)),
-            self.addImageButton.heightAnchor.constraint(equalToConstant: CGFloat(20)),
+//            self.addImageButton.heightAnchor.constraint(equalToConstant: CGFloat(20)),
+            
+            self.emailTextField.topAnchor.constraint(equalTo: self.addImageButton.bottomAnchor, constant: CGFloat(15)),
+            self.emailTextField.leadingAnchor.constraint(equalTo: self.addImageButton.leadingAnchor),
+            self.emailTextField.trailingAnchor.constraint(equalTo: self.addImageButton.trailingAnchor),
+            self.emailTextField.heightAnchor.constraint(equalToConstant: CGFloat(45)),
+            
+            self.passwordTextField.topAnchor.constraint(equalTo: self.emailTextField.bottomAnchor, constant: CGFloat(10)),
+            self.passwordTextField.leadingAnchor.constraint(equalTo: self.addImageButton.leadingAnchor),
+            self.passwordTextField.trailingAnchor.constraint(equalTo: self.addImageButton.trailingAnchor),
+            self.passwordTextField.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+            self.registerUserButton.topAnchor.constraint(equalTo: self.passwordTextField.bottomAnchor, constant: CGFloat(10)),
+            self.registerUserButton.leadingAnchor.constraint(equalTo: self.addImageButton.leadingAnchor),
+            self.registerUserButton.trailingAnchor.constraint(equalTo: self.addImageButton.trailingAnchor),
+            self.registerUserButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor)
 //            imageAddUser.topAnchor.constraint(equalTo: self.topAnchor),
 //            imageAddUser.leadingAnchor.constraint(equalTo: self.leadingAnchor),
 //            imageAddUser.trailingAnchor.constraint(equalTo: self.trailingAnchor),
